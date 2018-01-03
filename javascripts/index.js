0b1c7b6b43f04d6f94ebbd492317523d8080d21a
NikunjAgrawal <agrawaln@grinnell.edu>
2018-01-03 15:22:02 -0500

    adding drag drop menu

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,9 +1,18 @@
+for(var year = start; year <=end; year++){
+	options += "<option>"+year+"</option>"
+}
+document.getElementById("year").innerHTML = options
+
 dataPairings = document.getElementById("dataPairings")
 let makeListOfData = (data) => {
   years = data.map((entry) => {
     return "<tr><td>"+ entry.year + "</td>" + "<td>"+ entry.nyc_consumption_million_gallons_per_day + "</td></tr>"
+   
   })
   entryString = years.join("")
   dataPairings.innerHTML = "<tr><th>Year</th><th>Consumption (million gallons per day)</th></tr>" + entryString
 }
+var start = 1979
+var end = 2016
+var options = ""
 
