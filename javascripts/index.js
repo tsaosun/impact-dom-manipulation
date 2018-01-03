76c6eb01f588e711043c6beabf43278d302028aa
NikunjAgrawal <agrawaln@grinnell.edu>
2018-01-03 15:49:51 -0500

    updated index files

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,18 +1,23 @@
+var start = 1979
+var end = 2016
+var options = ""
 for(var year = start; year <=end; year++){
 	options += "<option>"+year+"</option>"
 }
+options = "<option> All years </option>" + options
 document.getElementById("year").innerHTML = options
 
 dataPairings = document.getElementById("dataPairings")
 let makeListOfData = (data) => {
   years = data.map((entry) => {
     return "<tr><td>"+ entry.year + "</td>" + "<td>"+ entry.nyc_consumption_million_gallons_per_day + "</td></tr>"
-   
   })
   entryString = years.join("")
   dataPairings.innerHTML = "<tr><th>Year</th><th>Consumption (million gallons per day)</th></tr>" + entryString
 }
-var start = 1979
-var end = 2016
-var options = ""
 
+submitEl = document.getElementById("submit")
+submitEl.onclick = () => {
+  value = document.getElementById("year").value
+  
+}
